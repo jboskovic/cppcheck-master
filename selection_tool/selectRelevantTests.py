f27f709464dd40a029d662e19ae6ecd45652e9cc
Jovana Boskovic <jboskovi@cisco.com>
2024-08-01 14:11:55 +0300

    set testing for selected tests

--- selection_tool/selectRelevantTests.py
+++ selection_tool/selectRelevantTests.py
@@ -23,6 +23,22 @@ class SelectRelevantTests:
         print("Selecteed tests")
         print(self.coverage_tool.output)
         print('###################################')
+        self.write_selected_test_to_file(self.coverage_tool.output)
+    
+    def write_selected_test_to_file(self, list_of_tests):
+        if list_of_tests != ["all"]:
+            file_name = project_name + "/selected_tests.txt"
+            print("Write selected tests to a file {}".format(file_name))
+            try:
+                with open(file_name, 'w') as file:
+                    for test in list_of_tests:
+                        file.write(f"{test}\n")
+                    print("Successfully wrote {} tests to {}".format(len(list_of_tests), file_name))
+            except Exception as e:
+                print(f"An error occurred: {e}")
+        else:
+            print("Tool for Selecting Relevant Tests selected all tests")
+        print('###################################')
 
 
 if __name__ == '__main__':
