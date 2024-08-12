5ee95fbc5f07ba17f9ca03841892caf60a2b821d
Jovana Boskovic <jboskovi@cisco.com>
2024-08-12 20:01:49 +0300

    fix file name

--- selection_tool/parsePR.py
+++ selection_tool/parsePR.py
@@ -75,6 +75,7 @@ class ParsePR:
                 line = line.strip()
                 if line.startswith('+++ '):
                     file_name = line.split('+++ ')[1][2:].strip()
+                    file_name = file_name[1:]
                     if not self.check_if_ext(file_name, coverage_extensions):
                         file_is_covered_with_coverage = False
                     else:
