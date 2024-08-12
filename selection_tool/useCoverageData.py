6d3b15deb6f64ff9625b1c909aa034943550ed71
Jovana Boskovic <jboskovi@cisco.com>
2024-08-12 23:32:30 +0300

    remove change in whole

--- selection_tool/useCoverageData.py
+++ selection_tool/useCoverageData.py
@@ -279,7 +279,7 @@ class CoverageData:
                     change_name = change_name.split("static ")[1]
                     print("Change name remove static ", change_name)
                 if count_of_spliter_original == count_of_spliter_found:
-                    if change_name + '(' in whole_name or change_name + '<' in whole_name or change_name in whole_name:
+                    if change_name + '(' in whole_name or change_name + '<' in whole_name:
                         print("Change function has similar names as whole name ")
                         print(change_name)
                         print(whole_name)
