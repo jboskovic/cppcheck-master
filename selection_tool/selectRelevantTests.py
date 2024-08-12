b7129ad04e9b5dec342e83d3bcdef07f6214ae60
Jovana Boskovic <jboskovi@cisco.com>
2024-08-12 11:22:41 +0300

    add sha fro saving selected data to the shared location

--- selection_tool/selectRelevantTests.py
+++ selection_tool/selectRelevantTests.py
@@ -1,9 +1,18 @@
 from parsePR import ParsePR
 from useCoverageData import CoverageData
 from helper_functions import *
+import argparse
+
+def parse_args(command_line_options):
+
+    argparser = argparse.ArgumentParser(description='Arguments for Select Relevant Tests script')
+    argparser.add_argument('--sha', dest='sha', type=str, action='store', help="Set sha for which we are running the PR selection")
+    return argparser.parse_args(command_line_options)
+
 
 class SelectRelevantTests:
-    def __init__(self):
+    def __init__(self, sha):
+        self.sha = sha
         print('Starting PR Parsing...')
         self.parser = ParsePR()
         print('PR Parsing finished successfully!')
@@ -25,12 +34,17 @@ class SelectRelevantTests:
     def write_selected_test_to_file(self, list_of_tests):
         if list_of_tests != ["all"]:
             file_name = project_name + "/selected_tests.txt"
+            file_for_evaluation = f"/var/jenkins_home/selections/selected_tests_{self.sha}.txt"
             print("Write selected tests to a file {}".format(file_name))
             try:
                 with open(file_name, 'w') as file:
                     for test in list_of_tests:
                         file.write(f"{test}\n")
                     print("Successfully wrote {} tests to {}".format(len(list_of_tests), file_name))
+                with open(file_for_evaluation, 'w') as file:
+                    for test in list_of_tests:
+                        file.write(f"{test}\n")
+                    print("Successfully wrote {} tests to {}".format(len(list_of_tests), file_for_evaluation))
             except Exception as e:
                 print(f"An error occurred: {e}")
         else:
@@ -39,4 +53,5 @@ class SelectRelevantTests:
 
 
 if __name__ == '__main__':
-    stage = SelectRelevantTests()
+    args = parse_args(sys.argv[1:])
+    stage = SelectRelevantTests(args.sha)
