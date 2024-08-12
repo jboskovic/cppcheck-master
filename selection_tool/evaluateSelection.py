9377696f2f77efeebf1d59792c419e38c841f10e
Jovana Boskovic <jboskovi@cisco.com>
2024-08-12 21:25:37 +0300

    Remove comments

--- selection_tool/evaluateSelection.py
+++ selection_tool/evaluateSelection.py
@@ -34,9 +34,7 @@ if __name__ == '__main__':
             print(f"File {file} doesnt have coverage")
             continue
         else:
-            print("File found ", file)
             file_indexed = str(files_indexed_files[file])
-            print("Index ", file_indexed)
         
         lines_to_tests = None
         if file_indexed not in lines_to_tests_file:
@@ -44,18 +42,13 @@ if __name__ == '__main__':
             continue
         else:
             lines_to_tests = lines_to_tests_file[file_indexed]
-        print(f"Covered lines for file {lines_to_tests} {file}")
         for line in lines:
-            print("Changed line number ", line)
             if str(line) in lines_to_tests:
-                print(f"Line is covered {line}")
                 covered_tests.extend(lines_to_tests[str(line)])
-                print(f"Found tests {lines_to_tests[str(line)]}")
 
     covered_tests_names = []
     for test, index in tests_indexed_filles.items():
         if index in covered_tests:
-            print(f"Index {index} in covered tests")
             covered_tests_names.append(test)
 
     print("Selected tests")
